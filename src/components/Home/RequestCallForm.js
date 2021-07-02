ce47c0e292cb4ecdf9abb791fe7de88f17893609
arseniy-poltev <hgh141592_71@outlook.com>
2021-07-02 09:34:24 +0700

    fix fonts, clean up homepage

--- src/components/Home/RequestCallForm.js
+++ src/components/Home/RequestCallForm.js
@@ -1,24 +1,35 @@
 import React from "react"
+import PhoneIcon from "../../assets/Icons/phone.svg"
+import UserIcon from "../../assets/Icons/user.svg"
 
 export default function RequestCallForm({ text }) {
   return (
-    <div className="request-call-form">
+    <div className="call-form">
       <h3>{text}</h3>
-      <div className="d-flex justify-content-between">
-        <input
-          className="input-outlined"
-          name="firstname"
-          type="text"
-          placeholder="First Name"
-        />
-        <input
-          className="input-outlined"
-          name="phonenumber"
-          type="text"
-          placeholder="Phone Number"
-        />
+      <div>
+        <div className="call-form__input-wrapper">
+          <UserIcon />
+          <input
+            className="input-outlined"
+            name="firstname"
+            type="text"
+            placeholder="First Name"
+          />
+        </div>
+        <div className="call-form__input-wrapper">
+          <PhoneIcon />
+          <input
+            className="input-outlined"
+            name="phonenumber"
+            type="text"
+            placeholder="Phone Number"
+          />
+        </div>
       </div>
-      <button className="btn btn-primary">Request a call</button>
+      <button className="btn btn-primary">
+        <PhoneIcon />
+        Request a call
+      </button>
     </div>
   )
 }
