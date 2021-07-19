d7ff553bcac2edc856b02f3796687420b3b4def8
arseniy-poltev <hgh141592_71@outlook.com>
2021-07-20 00:07:33 +0700

    add fomr validation

--- src/components/common/RequestCallForm.js
+++ src/components/common/RequestCallForm.js
@@ -1,35 +1,65 @@
 import React from "react"
 import PhoneIcon from "../../assets/Icons/phone.svg"
 import UserIcon from "../../assets/Icons/user.svg"
+import { useForm, Controller } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as Yup from "yup"
 
 export default function RequestCallForm({ className, text }) {
+  const validationSchema = Yup.object().shape({
+    firstName: Yup.string().required("First name is required"),
+    phoneNumber: Yup.string().required("Phone number is required"),
+  })
+  const formOptions = { resolver: yupResolver(validationSchema) }
+  const {
+    register,
+    handleSubmit,
+    trigger,
+    setValue,
+    control,
+    formState,
+  } = useForm(formOptions)
+  const { errors, isValid } = formState
+
+  function onSubmit(formData) {
+    console.log("RequestCallForm->onSubmit", formData)
+    alert(JSON.stringify(formData))
+  }
+
   return (
-    <div className={`call-form ${className}`}>
+    <form
+      className={`call-form ${className}`}
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <h3>{text}</h3>
       <div>
         <div className="call-form__input-wrapper">
           <UserIcon />
           <input
+            {...register("firstName")}
             className="input-outlined"
-            name="firstname"
+            name="firstName"
             type="text"
             placeholder="First Name"
           />
+          <span className="span-error">{errors.firstName?.message}</span>
         </div>
         <div className="call-form__input-wrapper">
           <PhoneIcon />
           <input
+            {...register("phoneNumber")}
             className="input-outlined"
-            name="phonenumber"
+            name="phoneNumber"
             type="text"
             placeholder="Phone Number"
           />
+          <span className="span-error">{errors.phoneNumber?.message}</span>
         </div>
       </div>
-      <button className="btn btn-primary">
+      <button className="btn btn-primary" onClick={handleSubmit}>
         <PhoneIcon />
         Request a call
       </button>
-    </div>
+    </form>
   )
 }
