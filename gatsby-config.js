c2cd39157ee2c780c6b2d33661649afc4c23ad1b
arseniy-poltev <hgh141592_71@outlook.com>
2021-10-22 01:55:32 +0700

    commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,15 +59,14 @@ let gatsbyGTMOptions = {
   defaultDataLayer: { platform: "gatsby" },
 
   // Specify optional GTM environment details.
-  gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
-  gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-  dataLayerName: "YOUR_DATA_LAYER_NAME",
+  // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+  // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+  // dataLayerName: "YOUR_DATA_LAYER_NAME",
 
   // Name of the event that is triggered
   // on every Gatsby route change.
-  //
   // Defaults to gatsby-route-change
-  routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
+  // routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
   // Defaults to false
   enableWebVitalsTracking: true,
 }
